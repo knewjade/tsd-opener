f727700fb5fd7b16d7f7024b451fd9b6b01d78f2
knewjade <knewjade@gmail.com>
2019-08-19 21:42:30 +0900

    Create Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,24 @@
 <template>
-    <v-content>
+    <v-container>
         <h3>開幕直後の7ミノで組めるTSD一覧</h3>
 
-        <div>
-            「ツモ順」と「そのツモ順で組めるTSD」が載っています。<br/>
-        </div>
+        <v-container>
+            「ツモ順」と「そのツモ順で組めるTSDの地形」が載っています。<br>
+            比較的、ページが重いのでご注意ください。
+        </v-container>
 
-        <router-link to="/main">メイン画面にすすむ</router-link>
-    </v-content>
+        <h4>メニューの説明</h4>
+
+        <ul class="menu-description">
+            <li>Harddrop = オンのとき、T以外をハードドロップのみで組める地形を表示します</li>
+            <li>Hold = オンのとき、T以外をホールドしないと組めない地形も表示します</li>
+            <li>AirTSD = オンのとき、Tが地面から離れている地形も表示します</li>
+        </ul>
+
+        <v-container>
+            <router-link to="/main">メイン画面にすすむ</router-link>
+        </v-container>
+    </v-container>
 </template>
 
 <script lang="ts">
@@ -18,3 +29,9 @@ import { Component } from 'vue-property-decorator';
 export default class Home extends Vue {
 }
 </script>
+
+<style scoped>
+.menu-description {
+    margin-left: 40px;
+}
+</style>
